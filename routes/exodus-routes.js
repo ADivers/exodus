714a709919361ab802a7c257671ef8e2f8ee057f
Carl Dor <carljdor@gmail.com>
2017-09-15 21:05:30 -0400

    added email list functionality

--- routes/exodus-routes.js
+++ routes/exodus-routes.js
@@ -43,13 +43,6 @@ module.exports = function(app) {
       var fromEmail = 'exoduscrmtest@gmail.com'; // add email of the gmail you are sending from
       var password =  'exodustest'  // add gmail password
       
-      emailTo = [
-          'divers1776@gmail.com',
-          'carljdor@gmail.com',
-          'ajnwosu@gmail.com',
-          'm11farrelly@gmail.com'
-      ]
-      
       var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
